63f9c8b406df96280a2aa7c52b1bab6425547840
ESHWAR <chalamallan3@gmail.com>
2024-08-31 01:03:24 +0530

    updated server

--- server/database/db.js
+++ server/database/db.js
@@ -1,5 +1,5 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,22 +7,24 @@ const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
 const Connection = () => {
+  const MONGODB_URI = `mongodb+srv://root:${PASSWORD}@atlascluster.mvc3o7d.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
 
-    const MONGODB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-hphkfqv-shard-00-00.bmuzxat.mongodb.net:27017,ac-hphkfqv-shard-00-01.bmuzxat.mongodb.net:27017,ac-hphkfqv-shard-00-02.bmuzxat.mongodb.net:27017/?ssl=true&replicaSet=atlas-ifygh4-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+  mongoose.connection.on("connected", () => {
+    console.log("Database connected successfully");
+  });
 
-    mongoose.connection.on('connected', () => {
-        console.log('Database connected Successfully');
-    })
+  mongoose.connection.on("disconnected", () => {
+    console.log("Database disconnected");
+  });
 
-    mongoose.connection.on('disconnected', () => {
-        console.log('Database disconnected');
-    })
-
-    mongoose.connection.on('error', () => {
-        console.log('Error while connecting with the database ', error.message);
-    })
-}
+  mongoose.connection.on("error", (err) => {
+    console.log("Error while connecting with the database:", err.message);
+  });
+};
 
 export default Connection;
